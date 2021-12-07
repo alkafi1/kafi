d87e687878aebe4f095587a9d0e26f95d8bfcfdd
Md.Al kafi <49485037+alkafi1@users.noreply.github.com>
2021-12-07 10:12:16 +0600

    Add all file

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {myFunction()};
+
+var nav = document.getElementById("nav");
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
